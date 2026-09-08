a2a702f8e294efded179e097547ebf492f182e33
mf103871-boop <mf103871@gmail.com>
2026-09-08 19:51:56 +0300

    bank: add 8 movie and anime categories (1920 questions)
    Register movies, arabmovies, AOT, Death Note, Demon Slayer, Jujutsu Kaisen, Hunter x Hunter, and Spacetoon packs; validator passes with no errors.

--- src/data/categories/index.js
+++ src/data/categories/index.js
@@ -24,15 +24,23 @@ import pack_dialects from './dialects.json' with { type: 'json' };
 import pack_books from './books.json' with { type: 'json' };
 import pack_arabmusic from './arabmusic.json' with { type: 'json' };
 import pack_theater from './theater.json' with { type: 'json' };
+import pack_movies from './movies.json' with { type: 'json' };
+import pack_arabmovies from './arabmovies.json' with { type: 'json' };
 import pack_actors from './actors.json' with { type: 'json' };
 import pack_anime from './anime.json' with { type: 'json' };
 import pack_naruto from './naruto.json' with { type: 'json' };
 import pack_onepiece from './onepiece.json' with { type: 'json' };
 import pack_dragonball from './dragonball.json' with { type: 'json' };
+import pack_aot from './aot.json' with { type: 'json' };
+import pack_deathnote from './deathnote.json' with { type: 'json' };
+import pack_demonslayer from './demonslayer.json' with { type: 'json' };
+import pack_jujutsu from './jujutsu.json' with { type: 'json' };
+import pack_hunterxhunter from './hunterxhunter.json' with { type: 'json' };
+import pack_spacetoon from './spacetoon.json' with { type: 'json' };
 import pack_videogames from './videogames.json' with { type: 'json' };
 import pack_puzzles from './puzzles.json' with { type: 'json' };
 
-export const CATS = [pack_general, pack_geo, pack_capitals, pack_science, pack_animals, pack_history, pack_food, pack_fruitsveg, pack_sports, pack_football, pack_worldcup, pack_tech, pack_apps, pack_brands, pack_cars, pack_shopping, pack_arabic, pack_proverbs, pack_dialects, pack_books, pack_arabmusic, pack_theater, pack_actors, pack_anime, pack_naruto, pack_onepiece, pack_dragonball, pack_videogames, pack_puzzles];
+export const CATS = [pack_general, pack_geo, pack_capitals, pack_science, pack_animals, pack_history, pack_food, pack_fruitsveg, pack_sports, pack_football, pack_worldcup, pack_tech, pack_apps, pack_brands, pack_cars, pack_shopping, pack_arabic, pack_proverbs, pack_dialects, pack_books, pack_arabmusic, pack_theater, pack_movies, pack_arabmovies, pack_actors, pack_anime, pack_naruto, pack_onepiece, pack_dragonball, pack_aot, pack_deathnote, pack_demonslayer, pack_jujutsu, pack_hunterxhunter, pack_spacetoon, pack_videogames, pack_puzzles];
 
 export const CATEGORY_IDS = CATS.map((category) => category.id);
 export const TOTAL_QUESTIONS = CATS.reduce((sum, category) => sum + category.qs.length, 0);
